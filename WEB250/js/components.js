15c3ca47487dc58735ef4202c41e9d746d238633
stevensharman1 <1stevensharman@gmail.com>
2026-08-24 18:39:27 -0400

    Add async function to include HTML components

--- WEB250/js/components.js
+++ WEB250/js/components.js
@@ -1,1 +1,20 @@
+async function includeComponent(elementId, file) {
+    const element = document.getElementById(elementId);
 
+    try {
+        const response = await fetch(file);
+
+        if (!response.ok) {
+            throw new Error(`Could not load ${file}`);
+        }
+
+        element.innerHTML = await response.text();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    includeComponent("header", "components/header.html");
+    includeComponent("footer", "components/footer.html");
+});
