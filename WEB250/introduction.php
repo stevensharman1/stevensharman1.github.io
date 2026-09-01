334d0556a08041635a3dfce313ee7c36fc1892e1
stevensharman1 <1stevensharman@gmail.com>
2026-09-01 15:07:58 -0400

    Add introduction content to introduction.php

--- WEB250/introduction.php
+++ WEB250/introduction.php
@@ -1,1 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Steven Sharman’s Sneaky Snake ✦ WEB250 ✦ Introduction</title>
+</head>
 
+<body>
+     <div id="header"></div>
+
+
+    <main>
+        <h2>Introduction</h2>
+
+        <p>
+            Hello, my name is Steven. I am currently taking my last few classes
+            before I graduate with my associate’s degree. I moved to Charlotte
+            four years ago for college. I currently work full-time as a
+            Frozen/Dairy manager.
+        </p>
+
+        <ul>
+            <li>
+                <strong>Personal Background:</strong>
+                I’ve lived all around the southern states, but I moved to
+                Charlotte four years ago.
+            </li>
+
+            <li>
+                <strong>Professional Background:</strong>
+                I currently work full-time as a Frozen/Dairy manager.
+            </li>
+
+            <li>
+                <strong>Academic Background:</strong>
+                I graduate in December with my associate’s degree.
+            </li>
+
+            <li>
+                <strong>Primary Work Computer:</strong>
+                IdeaPad Slim 3, Windows 11.
+            </li>
+
+            <li>
+                <strong>Primary Work Location:</strong>
+                My desk in my apartment.
+            </li>
+
+            <li>
+                <strong>Alternate Computer &amp; Location:</strong>
+                I can use my girlfriend’s computer if needed.
+            </li>
+
+            <li>
+                <strong>Courses I’m Taking, &amp; Why:</strong>
+                <ul>
+                    <li>
+                        <strong>WEB250 - Database Driven Websites:</strong>
+                        Needed for my program, and it sounded interesting.
+                    </li>
+
+                    <li>
+                        <strong>CTS240 - Project Management:</strong>
+                        Needed for my program, and project management is
+                        crucial in any field.
+                    </li>
+
+                    <li>
+                        <strong>CTI120 - Network &amp; Security Foundations:</strong>
+                        Needed for my program.
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <strong>Funny/Interesting Item to Remember Me By:</strong>
+                I have a Pitbull Chihuahua Mix. He’s basically a 45 lb.
+                Chihuahua with a boxy head.
+            </li>
+        </ul>
+
+        <blockquote>
+            “We are made of star-stuff.”
+            <br>
+            – Carl Sagan
+        </blockquote>
+    </main>
+
+    <div id="footer"></div>
+    <script src="js/components.js"></script>
+</body>
+</html>
