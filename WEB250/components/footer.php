9a4e1cab0ce3261d7daec3f82f52ce46d9c569ed
stevensharman1 <1stevensharman@gmail.com>
2026-09-01 15:17:40 -0400

    Add footer with links to GitHub and GitHub.io

--- WEB250/components/footer.php
+++ WEB250/components/footer.php
@@ -1,1 +1,7 @@
-
+<footer>
+    <p>
+        <a href="https://github.com/stevensharman1">GitHub</a> |
+        <a href="https://stevensharman1.github.io">GitHub.io</a> |
+        <a href="https://stevensharman1.github.io/WEB250">WEB250</a>
+    </p>
+</footer>
