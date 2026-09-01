e14cd765d16fe6a3b092c4fa59c0771f85879ff7
stevensharman1 <1stevensharman@gmail.com>
2026-09-01 15:07:10 -0400

    Update contract.php

--- WEB250/contract.php
+++ WEB250/contract.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Steven Sharman’s Sneaky Snake ✦ WEB250 ✦ Contract</title>
+</head>
 
+<body>
+    <div id="header"></div>
+
+    <main>
+        <h2>Contract</h2>
+
+        <p>
+            I fully understand that this is a publicly accessible github page, and that all my work is appropriate and origional.
+        </p>
+    </main>
+
+     <div id="footer"></div>
+    <script src="js/components.js"></script>
+</body>
+</html>
